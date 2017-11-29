f4d471de876fb6d174ffcb0b93a0de8fa132bc1d
ekosp <ekosetyopurnomo@gmail.com>
2017-11-29 20:22:16 +0700

    init : Initial Commit
    From here, project already can passing joke from java library to android library, also integrated by google cloud endpoint

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
@@ -0,0 +1,12 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by Eko Setyo Purnomo on 29-Nov-17.
+ * Find me on https://ekosp.com
+ * Or email me directly to ekosetyopurnomo@gmail.com
+ */
+
+import com.ekosp.joketeller.backend.myApi.MyApi;
+
+public class EndpointAsyncTask {
+}
---
+++ javalib/src/main/java/com/example/JokeProvider.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class JokeProvider {
+    public String tellJoke() {
+        return "Microsoft gives you Windows, Linux gives you a home!";
+    }
+}
